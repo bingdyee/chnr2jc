5b722a839618a8781f5cbda481d6d3ec895497cf
e <fetaxyu@gmail.com>
2019-07-25 08:12:26 +0800

    update tip info

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from core.generator import CodeGenerator
 def usage():
     parser = optparse.OptionParser("usage: %prog -t <table name> -m <module> -p <package name> [-o target output dir] "
                                    "[-u author]")
-    parser.add_option("-t", dest="table_name", type="string", help="specify table")
-    parser.add_option("-m", dest="module", type="string", help="specify module name")
-    parser.add_option("-p", dest="package_name", type="string", help="specify package name")
-    parser.add_option("-o", dest="target", type="string", help="specify package name", default='temp')
-    parser.add_option("-u", dest="author", type="string", help="specify package name", default='devuser')
+    parser.add_option("-t", dest="table_name", type="string", help="table name")
+    parser.add_option("-m", dest="module", type="string", help="module name")
+    parser.add_option("-p", dest="package_name", type="string", help="package name")
+    parser.add_option("-o", dest="target", type="string", help="package name", default='temp')
+    parser.add_option("-u", dest="author", type="string", help="author name", default='devuser')
     (options, args) = parser.parse_args()
     if options.table_name is None or options.module is None or options.package_name is None:
         parser.print_help()
